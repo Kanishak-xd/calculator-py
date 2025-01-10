3148af312acdb46d993718221c850d008eeb87ad
Kanishak-xd <kanishak.sharma22@st.niituniversity.in>
2025-01-10 15:52:48 +0530

    0 division bug fixed

--- Division.py
+++ Division.py
@@ -1,4 +1,8 @@
 # This function adds two numbers
 def divide(x, y):
-    return x / y
-
+    try:
+        result = x / y
+    except ZeroDivisionError:
+        return "Error: Denominator can't be zero."
+    else:
+        return result

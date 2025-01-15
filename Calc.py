c06b53dd6dd86d7400cd5ea5313565a274b51312
Kanishak-xd <kanishak.sharma22@st.niituniversity.in>
2025-01-15 14:00:06 +0530

    fixed calculator

--- Calc.py
+++ Calc.py
@@ -2,21 +2,23 @@ import Addition
 import Subtraction
 import Multiplication
 import Division
-
+import Exponent
+import Modulus
 
 print("Select operation.")
 print("1. Add")
 print("2. Subtract")
 print("3. Multiply")
 print("4. Divide")
-
+print("5. Exponent")
+print("6. Modulus")
 
 while True:
     # take input from the user
-    choice = input("Enter choice(1/2/3/4): ")
+    choice = input("Enter choice(1/2/3/4/5/6): ")
 
-    # check if choice is one of the four options
-    if choice in ('1', '2', '3', '4'):
+    # check if choice is one of the options
+    if choice in ('1', '2', '3', '4', '5', '6'):
         try:
             num1 = float(input("Enter first number: "))
             num2 = float(input("Enter second number: "))
@@ -35,12 +37,17 @@ while True:
 
         elif choice == '4':
             print(num1, "/", num2, "=", Division.divide(num1, num2))
-        
+
+        elif choice == '5':
+            print(num1, "^", num2, "=", Exponent.exp(num1, num2))
+
+        elif choice == '6':
+            print(num1, "%", num2, "=", Modulus.mod(num1, num2))
+
         # check if user wants another calculation
         # break the while loop if answer is no
         next_calculation = input("Let's do next calculation? (yes/no): ")
-        if next_calculation == "no":
-          break
+        if next_calculation.lower() == "no":
+            break
     else:
         print("Invalid Input")
-
--- Modulus.py
+++ Modulus.py
@@ -1,4 +1,4 @@
-def calculate_modulus(num1, num2):
+def mod(num1, num2):
     if num2 == 0:
         return "Division by zero is not allowed."
     return num1 % num2
